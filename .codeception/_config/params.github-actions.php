a90ae629fade774f7b51e85663e2f9c8ff10d997
Maxym Denysenko <mdenisenko95@gmail.com>
2020-08-09 17:20:49 +0300

    Add params for GH actions

---
+++ .codeception/_config/params.github-actions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * GH Actions parameters
+ *
+ * @since   {VERSION}
+ * @link    https://github.com/wppunk/WPPlugin
+ * @license GPLv2 or later
+ * @package PluginName
+ * @author  WPPunk
+ */
+
+return [
+	'WP_URL'            => 'http://plugin-name.test',
+	'WP_ADMIN_USERNAME' => 'admin',
+	'WP_ADMIN_PASSWORD' => 'admin',
+	'WP_ADMIN_PATH'     => '/wp-admin',
+	'DB_HOST'           => '127.0.0.1',
+	'DB_NAME'           => 'test_db',
+	'DB_USER'           => 'user',
+	'DB_PASSWORD'       => 'passw0rd',
+	'DB_TABLE_PREFIX'   => 'wp_',
+];
