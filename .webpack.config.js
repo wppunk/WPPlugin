6e010846db4325fc0606a78698d35e47018d518a
Maxym Denysenko <mdenisenko95@gmail.com>
2020-12-17 01:56:16 +0200

    Move from PostCSS to SCSS

--- .webpack.config.js
+++ .webpack.config.js
@@ -4,28 +4,20 @@ if ( ! Encore.isRuntimeEnvironmentConfigured() ) {
 	Encore.configureRuntimeEnvironment( process.env.NODE_ENV || 'dev' );
 }
 
-Encore.setOutputPath( 'assets/build' )
+Encore
+	.setOutputPath( 'assets/build' )
 	.setPublicPath( '/' )
 	.addEntry( 'js/admin/settings', './assets/.src/js/admin/settings/app.js' )
 	.addEntry( 'js/main', './assets/.src/js/front/main/app.js' )
-	.addStyleEntry(
-		'css/admin/settings',
-		'./assets/.src/pcss/admin/settings.pcss'
-	)
-	.addStyleEntry( 'css/main', './assets/.src/pcss/main.pcss' )
+	.addStyleEntry( 'css/admin/settings', './assets/.src/scss/admin/settings.scss' )
+	.addStyleEntry( 'css/main', './assets/.src/scss/main.scss' )
 	.splitEntryChunks()
 	.disableSingleRuntimeChunk()
 	.cleanupOutputBeforeBuild()
 	.enableBuildNotifications()
 	.enableSourceMaps( ! Encore.isProduction() )
-	.enablePostCssLoader()
-	.configureLoaderRule( 'css', ( loaderRule ) => {
-		loaderRule.test = /\.(css|p(ost)?css)$/;
-	} )
-	.copyFiles( {
-		from: './assets/.src/img',
-		to: './img/[path][name].[ext]'
-	} )
+	.enableSassLoader()
+	.copyFiles( { from: './assets/.src/img', to: './img/[path][name].[ext]' } )
 	.enableEslintLoader();
 
 module.exports = Encore.getWebpackConfig();
