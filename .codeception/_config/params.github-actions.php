be8a007c3aaf81dd9c79a91be8697a2ce7f5e63e
Maxym Denysenko <mdenisenko95@gmail.com>
2021-08-16 00:08:05 +0300

    Fix credentials

--- .codeception/_config/params.github-actions.php
+++ .codeception/_config/params.github-actions.php
@@ -15,7 +15,7 @@ return [
 	'WP_ADMIN_PASSWORD' => getenv( 'WP_ADMIN_PASSWORD' ),
 	'WP_ADMIN_PATH'     => getenv( 'WP_ADMIN_PATH' ),
 	'DB_HOST'           => getenv( 'DB_HOST' ),
-	'DB_NAME'           => getenv( 'DB_HOST' ),
+	'DB_NAME'           => getenv( 'DB_NAME' ),
 	'DB_USER'           => getenv( 'DB_USER' ),
 	'DB_PASSWORD'       => getenv( 'DB_PASSWORD' ),
 	'DB_TABLE_PREFIX'   => getenv( 'DB_TABLE_PREFIX' ),
